dfcbd9a2700f530563388ba6c5cd87123da3976d
zhengdan_nacy@163.com <zhengdan_nacy@163.com>
2019-02-13 17:37:18 +0800

    libimagepipeline.so 64位加载成32位的崩溃修改，在android项目根目录下的build.gradle中的defaultConfig节点添加packagingOptions配置

--- App/view/FadeInViewTest.js
+++ App/view/FadeInViewTest.js
@@ -5,11 +5,14 @@ import FadeInView from '../AnimateUtils/FadeInView';
 export default class FadeInViewTest extends React.Component{
 
     render() {
-        <View style={{ alignItems: 'center', justifyContent: 'center'}}>
-            <FadeInView style={{width: 250, height: 50, backgroundColor: 'powderblue'}}>
-                <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}>Fading in</Text>
-            </FadeInView>
-        </View>
+        return(
+            <View style={{ alignItems: 'center', justifyContent: 'center'}}>
+                <FadeInView style={{width: 250, height: 50, backgroundColor: 'powderblue'}}>
+                    <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}>Fading in</Text>
+                </FadeInView>
+            </View>
+            )
+
     }
 
 }
--- App/view/JumpsView.js
+++ App/view/JumpsView.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import {TouchableHighlight, Text,View,StyleSheet} from 'react-native'
+import {TouchableHighlight, Text,View,StyleSheet,Button} from 'react-native'
 import MyToastUtils from "../utils/MyToastUtils";
+
 
 export default class JumpsView extends  React.Component {
     // static navigationOptions = {
@@ -20,22 +21,32 @@ export default class JumpsView extends  React.Component {
             {/**/}
         {/*</View>*/}
 
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>JumpsView Screen</Text>
-            <Button
-                title="Go to Details... again"
-                onPress={() => this.props.navigation.push('Details')}
-            />
-            <Button
-                title="Go to Home"
-                onPress={() => this.props.navigation.navigate('Home')}
-            />
-            <Button
-                title="Go back"
-                onPress={() => this.props.navigation.goBack()}
-            />
-        </View>
+        return(
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <Text>JumpsView Screen</Text>
+                <Button
+                    title="Go to FadeInViewTest... again"
+                    // onPress={() => this.props.navigation.push('FadeInView')}
+                    onPress={() => this.props.navigation.navigate('FadeInViewTest')}
+                    // onPress={gotoFadeInView}
+                />
+                <Button
+                    title="Go to MoviesListView"
+                    onPress={() => this.props.navigation.navigate('MoviesListView')}
+                    // onPress={gotoMoviesListView}
+                />
+                <Button
+                    title="Go back"
+                    onPress={() => this.props.navigation.goBack()}
+                    // onPress={gotoBack}
+                />
+            </View>
+
+        )
+
+
     }
+
 
     // pressShowToast = () => {
     //     console.log("进入 pressShowToast，点击一下之前");
@@ -51,7 +62,16 @@ export default class JumpsView extends  React.Component {
 }
 
 
-
+// function gotoFadeInView(){
+//     this.props.navigation.navigate('FadeInView');
+// }
+// function gotoMoviesListView(){
+//     this.props.navigation.navigate('MoviesListView')
+// }
+//
+// function gotoBack(){
+//     this.props.navigation.goBack()
+// }
 const styles = StyleSheet.create({
     container:{
         flex: 1,
